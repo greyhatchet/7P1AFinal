a0c75c6f1ca6b942ac11f55fb79cda921987c3bb
alyssapacleb <42876282+alyssapacleb@users.noreply.github.com>
2018-11-29 14:09:10 -0600

    Display

--- scrollPractice.py
+++ scrollPractice.py
@@ -4,143 +4,168 @@ import sys
 import os
 import random
 
-num_players = 0
-player_list = []
+RED = (255, 0, 0)
+
 
 def events():
-	for event in pygame.event.get():
-		if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-			pygame.quit()
-			sys.exit()
-		elif event.type == KEYDOWN and event.key == K_SPACE:
-			return True
-		elif event.type == KEYDOWN and event.key == K_r:
-			return False
-		elif event.type == KEYDOWN and event.key == K_a:
-			return "y"
-	return 
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            pygame.quit()
+            sys.exit()
+        elif event.type == KEYDOWN and event.key == K_SPACE:
+            return True
+        elif event.type == KEYDOWN and event.key == K_r:
+            return False
+        elif event.type == KEYDOWN and event.key == K_a:
+            return "y"
+    return
+
 
 class BoardPiece():
-	def __init__ (self, color, playerNumber):
-		self.color = color
-		self.piece = playerNumber
-		self.xCord = 33
-		self.yCord = 277 + (playerNumber - 1) * 20;
-		self.absX = self.xCord
-		self.absY = self.yCord
+    def __init__(self, color, playerNumber):
+        self.color = color
+        self.piece = playerNumber
+        self.xCord = 33
+        self.yCord = 277 + (playerNumber - 1) * 20
+        self.absX = self.xCord
+        self.absY = self.yCord
 
-	def draw(self,background):
-		pygame.draw.circle(background, self.color, (self.absX,self.absY),10,0)
+    def draw(self, background):
+        pygame.draw.circle(background, self.color, (self.absX, self.absY), 10, 0)
 
-	def diceRoll(self,x):
-		#check position on board and move piece appropriately
-		if self.xCord < 33 + (7 *44) or (self.xCord > 33 + (7*44) and self.xCord < 33 + (14*44) + 57):
-			self.absX += 44
-			self.xCord += 44
-		elif self.xCord == 33 + (7*44):
-			self.absX += 57
-			self.xCord += 57
-			self.absY -= 115
-		elif self.xCord == 33 + (14*44) + 57:
-			self.absX += 57
-			self.xCord += 57
-			self.absY += 115
-		elif self.xCord > 33 + (14*44) + 57 and self.xCord < 33 + (26*44) + 57:
-			self.absX += 44
-			self.xCord += 44
-		else:
-			self.xCord = 33
-			self.absX = self.xCord + x
-		
+    def diceRoll(self, x):
+        # check position on board and move piece appropriately
+        if self.xCord < 33 + (7 * 44) or (self.xCord > 33 + (7 * 44) and self.xCord < 33 + (14 * 44) + 57):
+            self.absX += 44
+            self.xCord += 44
+        elif self.xCord == 33 + (7 * 44):
+            self.absX += 57
+            self.xCord += 57
+            self.absY -= 115
+        elif self.xCord == 33 + (14 * 44) + 57:
+            self.absX += 57
+            self.xCord += 57
+            self.absY += 115
+        elif self.xCord > 33 + (14 * 44) + 57 and self.xCord < 33 + (26 * 44) + 57:
+            self.absX += 44
+            self.xCord += 44
+        else:
+            self.xCord = 33
+            self.absX = self.xCord + x
 
-	def worldScroll(self,scrollCord):
-		self.absX = self.absX + scrollCord
-
-
-def setNumPlayers(player_num):
-	global num_players
-	num_players = player_num
+    def worldScroll(self, scrollCord):
+        self.absX = self.absX + scrollCord
 
 
-def showBoard():
+# define display surface
+W, H = 800, 700
+HW, HH = W / 2, H / 2
+AREA = W * H
 
-	global num_players
-	global player_list
+os.environ['SDL_VIDEO_WINDOW_POS'] = "50,50"
 
-	# define display surface
-	W, H = 800, 700
-	HW, HH = W / 2, H / 2
-	AREA = W * H
-
-	os.environ['SDL_VIDEO_WINDOW_POS'] = "50,50"
-
-	# setup pygame
-	pygame.init()
-	CLOCK = pygame.time.Clock()
-	DS = pygame.display.set_mode((W, H))
-	pygame.display.set_caption("Game Board")
-	FPS = 120
-
-	#player objects
-
-	for i in range(num_players):
-		new_player = BoardPiece((i * 80, i * 80, i * 80), i+1)
-		player_list.append(new_player)
-
-	#very background image
-	wayBack = pygame.image.load("SeemsCool.jpg").convert()
+# setup pygame
+pygame.init()
+CLOCK = pygame.time.Clock()
+DS = pygame.display.set_mode((W, H))
+pygame.display.set_caption("Game Board")
+FPS = 120
 
 
-	bkgd = pygame.image.load("board.png").convert_alpha()
-	bkgd = pygame.transform.scale(bkgd,(1300,600))
-	x = 0
+def message_to_screen(msg, color, x_displace=0, y_displace=0, font_size=0):
+    nice_font = pygame.font.Font('freesansbold.ttf', font_size)
+    textSurface = nice_font.render(msg, True, color)
+    textSurf, textRect = textSurface, textSurface.get_rect()
+    textRect.center = (W / 2) + x_displace, (H / 2) + y_displace
+    DS.blit(textSurf, textRect)
+def text_objects(text, font):
+    textSurface = font.render(text, True, RED)
+    return textSurface, textSurface.get_rect()
 
-	#number of player switches
-	n = 0
+# player objects
 
-	roundCount = 1
-	# main loop
-	while True:
+p1 = BoardPiece((0, 0, 0), 1)
+p2 = BoardPiece((100, 100, 100), 2)
+p3 = BoardPiece((200, 200, 200), 3)
+p4 = BoardPiece((255, 255, 255), 4)
 
-		#background blit
-		DS.blit(wayBack,(0,0))
-		z = events()
+playerList = [p1, p2, p3, p4]
 
-		# relative x value
-		rel_x = x % bkgd.get_rect().width
-		DS.blit(bkgd, (rel_x - bkgd.get_rect().width, 60))
+# very background image
+wayBack = pygame.image.load("SeemsCool.jpg").convert()
 
-		#number of Rounds
-		numRounds = n // 4
+dice = pygame.image.load('dice2.png').convert_alpha()
+# dice = pygame.transform.scale(dice, (75, 75))
 
-		if rel_x < W:
-			x = 0
-			DS.blit(bkgd, (0, 60))
+bkgd = pygame.image.load("board.png").convert_alpha()
+bkgd = pygame.transform.scale(bkgd, (1300, 600))
+x = 0
+# number of player switches
+n = 0
 
-			#update character locations upon snapping back to start
-			for player in player_list:
-				player.absx = player.xCord
+roundCount = 1
+# main loop
+while True:
 
-		#if spacebar is hit
-		if z == True:
-			dieNumber = random.randint(1,6)
-			print("player"+ str((n % num_players) + 1) + " rolls " + str(dieNumber))
-			for i in range(dieNumber):
-				player_list[(n % num_players)].diceRoll(x)
-			n += 1
+    # background blit
+    DS.blit(wayBack, (0, 0))
+    z = events()
 
-		elif z == False:
-			x -= 44
-			for player in player_list:
-				player.worldScroll(-44)
+    # relative x value
+    rel_x = x % bkgd.get_rect().width
+    DS.blit(bkgd, (rel_x - bkgd.get_rect().width, 60))
+    message_to_screen("Player " + str((n % 4) + 1) + " rolls ", RED, -290, -300, 24)
+    message_to_screen("Press space to roll", RED, -262, -270, 24)
+    message_to_screen("Press R to scroll", RED, -275, -240, 24)
+    # number of Rounds
+    numRounds = n // 4
 
-		elif z == "y" and x < 0:
-			x += 44
-			for player in player_list:
-				player.worldScroll(44)
+    if rel_x < W:
+        x = 0
+        DS.blit(bkgd, (0, 60))
 
-		for player in player_list:
-			player.draw(DS)
+        # update character locations upon snapping back to start
+        p1.absX = p1.xCord
+        p2.absX = p2.xCord
+        p3.absX = p3.xCord
+        p4.absX = p4.xCord
 
-		pygame.display.update()
-		CLOCK.tick(FPS)
+    # if spacebar is hit
+    if z == True:
+        dieNumber = random.randint(1, 6)
+
+        print("player" + str((n % 4) + 1) + " rolls " + str(dieNumber))
+        for i in range(dieNumber):
+            playerList[(n % 4)].diceRoll(x)
+        n += 1
+
+        mediumText = pygame.font.Font('mago3.ttf', 50)
+
+        TextSurf, TextRect = text_objects(str(dieNumber), mediumText)
+        TextRect.center = (360, 98)
+        DS.blit(dice, (250, 0))
+        DS.blit(TextSurf, TextRect)
+        pygame.display.update()
+        pygame.time.delay(500)
+
+    elif z == False:
+        x -= 44
+        p1.worldScroll(-44)
+        p2.worldScroll(-44)
+        p3.worldScroll(-44)
+        p4.worldScroll(-44)
+
+    elif z == "y" and x < 0:
+        x += 44
+        p1.worldScroll(44)
+        p2.worldScroll(44)
+        p3.worldScroll(44)
+        p4.worldScroll(44)
+
+    p1.draw(DS)
+    p2.draw(DS)
+    p3.draw(DS)
+    p4.draw(DS)
+
+    pygame.display.update()
+    CLOCK.tick(FPS)
